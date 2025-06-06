6b8798a623ab574fba71a93907f887cbdb6aa7f8
Adefemi Oseni <adefemigreat1995@gmail.com>
2025-06-06 10:17:21 +0100

    chore: implement core skills functions

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,10 @@
 import random
-# rand_list =
+rand_list = [random.randint(1, 20) for _ in range(10)]
 
-# list_comprehension_below_10 =
+list_comprehension_below_10 = [x for x in rand_list if x < 10]
 
-# list_comprehension_below_10 =
+def handler(x): # just for better readability
+    return x < 10
+
+# list_comprehension_below_10 = using filter
+list_comprehension_below_10_with_filter = list(filter(handler, rand_list))
